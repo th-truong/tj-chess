2d607461c238ec5a97ff35ce5bcaeda1a6c5bd70
DESKTOP-N58MB14\Thomas <thomas.truong@ucalgary.ca>
2020-10-31 13:35:45 -0600

    finish dataloader to iterate through .pgn paths on multiple workers

--- src/config.py
+++ src/config.py
@@ -7,7 +7,7 @@ from pathlib import Path
 
 # points to the location of the .pgn files from the lichess elite db from a reddit user
 # found at https://www.reddit.com/r/chess/comments/gz8acg/introducing_the_lichess_elite_database/
-LICHESS_DB = Path(r"D:/")
+LICHESS_DB = Path(r"D:\paper_repos\tj-chess\Lichess Elite Database")
 
 STOCKFISH_ENGINE_PATH = Path(r"D:\paper_repos\tj-chess\stockfish_20090216_x64_bmi2.exe")
 
