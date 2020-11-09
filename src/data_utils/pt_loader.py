7fd2257426a5ba1d08ea5d143da84ed5700a987c
DESKTOP-N58MB14\Thomas <thomas.truong@ucalgary.ca>
2020-11-08 20:39:29 -0700

    adjusted how the workers files are divided up for the data loader

--- src/data_utils/pt_loader.py
+++ src/data_utils/pt_loader.py
@@ -15,13 +15,8 @@ class MoveLoader(torch.utils.data.IterableDataset):
         if not dataset_path.exists():
             raise ValueError(f"{str(dataset_path)} does not exist, please configure config.py to point to a valid path.")
 
-        self.all_pgn_files = list(dataset_path.glob("*.pgn"))
-
-        pgn_file_sizes = [file.stat().st_size for file in self.all_pgn_files]
-        self.total_size = np.sum(np.array(pgn_file_sizes, dtype=np.uint))
-
-        self.start = 0
-        self.end = len(self.all_pgn_files)
+        self.all_pgn_files = list(dataset_path.glob("*.pgn"))  # order from newest -> oldest, which also happens to roughly be largest -> smallest files
+        self.all_pgn_files.reverse()
 
         # worker_pgn_files is reassigned in worker_init_fn if multiple workers are used
         self.worker_pgn_files = self.all_pgn_files
@@ -70,23 +65,10 @@ def worker_init_fn(worker_id):
 
     dataset = worker_info.dataset  # the dataset copy in this worker process
 
-    size_per_worker = dataset.total_size / worker_info.num_workers
+    # used to store the .pgn files for each worker
+    files_per_worker = [[] for _ in range(0, worker_info.num_workers)]
 
-    files_per_worker = []  # used to store the .pgn files for each worker
+    for i, pgn_file in enumerate(dataset.all_pgn_files):
+        files_per_worker[i % worker_info.num_workers].append(pgn_file)
 
-    # iterate through the dataset .pgn files and evenly partition into {num_workers} parts of equal size (memory)
-    files = []
-    current_total_size = np.array(0, dtype=np.uint)
-    for pgn_file in dataset.all_pgn_files:
-        if current_total_size > size_per_worker:
-            files_per_worker.append(files)
-            files = []
-            current_total_size = np.array(0, dtype=np.uint)
-        files.append(pgn_file)
-        current_total_size = current_total_size + pgn_file.stat().st_size
-
-    if len(files) > 0:  # append the files for the last worker if there are unassigned files left
-        files_per_worker.append(files)
-
-    # reassign the .pgn files for this worker's copy of the dataset
     dataset.worker_pgn_files = files_per_worker[worker_id]
