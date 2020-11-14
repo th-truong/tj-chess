a8051eed29426f054784aa72f4cb6f0f9f35a502
Josh Heinrichs <joshiheinrichs@gmail.com>
2020-11-14 14:07:04 -0600

    Split gui and train into subcommands

--- src/main.py
+++ src/main.py
@@ -17,16 +17,19 @@ from scripts import display_gui, train_first_tj_model
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--gui', action='store_true')
-    parser.add_argument('--training', action='store_true')
-    parser.add_argument('--log-dir', default=cfg.LOG_DIR)
-    parser.add_argument('--lichess-db', default=cfg.LICHESS_DB)
-    parser.add_argument('--model')
-    parser.add_argument('--stockfish-exe', default=shutil.which('stockfish'))
+    subparsers = parser.add_subparsers()
+
+    parser_gui = subparsers.add_parser('gui')
+    parser_gui.set_defaults(func=display_gui)
+    parser_gui.add_argument('--lichess-db', default=cfg.LICHESS_DB)
+    parser_gui.add_argument('--model')
+    parser_gui.add_argument('--stockfish-exe', default=shutil.which('stockfish'))
+
+    parser_train = subparsers.add_parser('train')
+    parser_train.set_defaults(func=train_first_tj_model)
+    parser_train.add_argument('--log-dir', default=cfg.LOG_DIR)
+    parser_train.add_argument('--lichess-db', default=cfg.LICHESS_DB)
+
     args = parser.parse_args()
 
-    if args.gui:
-        display_gui(args)
-
-    if args.training:
-        train_first_tj_model(args)
+    args.func(args)
