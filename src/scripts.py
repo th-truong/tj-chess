d84aca1b2988c78ebec9a16f11fc6e2a90a9fb05
Josh Heinrichs <joshiheinrichs@gmail.com>
2020-11-15 00:37:40 -0600

    Clean up imports and remove old code

--- src/scripts.py
+++ src/scripts.py
@@ -1,22 +1,12 @@
 import sys
-from pathlib import Path
-from PyQt5.QtWidgets import QApplication
-import torch
-from tqdm import tqdm
-from torch.utils.tensorboard import SummaryWriter
-import numpy as np
 
+import torch
+import numpy as np
+from PyQt5.QtWidgets import QApplication
 import chess.pgn
 
 import config as cfg
-
 from gui import pyqt_classes
-from data_utils import pt_loader
-
-from PyQt5.QtWidgets import (QWidget, QGridLayout, QVBoxLayout, QPushButton,
-                             QLabel, QTextEdit, QLineEdit, QFileDialog, QApplication,
-                             QListWidget, QPlainTextEdit, QMainWindow, QTabWidget,
-                             QCheckBox)
 
 
 def display_gui(args):
@@ -26,82 +16,6 @@ def display_gui(args):
     window.show()
 
     sys.exit(app.exec_())
-
-
-def train_first_tj_model(args):
-    # start tensorboard logging
-    writer = SummaryWriter(log_dir=args.log_dir)
-
-    # create model
-    model = create_tj_model()
-    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-    model.to(device)
-    model.train()
-
-    # set up network interpreter
-    interpreter = noi.NetInterpreter()
-
-    # load dataset objects
-    dataset = pt_loader.MoveLoader(args.lichess_db)
-    pt_dataloader = torch.utils.data.DataLoader(dataset, batch_size=cfg.BATCH_SIZE,
-                                                num_workers=cfg.LOADER_WORKERS, worker_init_fn=pt_loader.worker_init_fn)
-
-    # configure training parameters
-    learning_rate = cfg.LEARNING_RATE
-    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-
-    loss_fn_policy = torch.nn.CrossEntropyLoss()
-    loss_fn_value = torch.nn.CrossEntropyLoss()
-
-    num_steps = cfg.MAX_ITERATIONS
-
-    for current_step, out in tqdm(enumerate(pt_dataloader)):
-        if current_step == 0:
-            # set the learning rate very low for warm up
-            for param_group in optimizer.param_groups:
-                param_group['lr'] = learning_rate / 1000
-            scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=cfg.SCHEDULER_PATIENCE,
-                                                                   factor=cfg.SCHEDULER_FACTOR)
-        elif current_step == cfg.WARM_UP_STEPS:
-            for param_group in optimizer.param_groups:
-                param_group['lr'] = learning_rate
-            scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=cfg.SCHEDULER_PATIENCE,
-                                                                   factor=cfg.SCHEDULER_FACTOR)
-        moves = out[0].to(device)
-        targets = {k: v.to(device) for k, v in out[1].items()}
-
-        pred_policies, pred_values, targets = model(moves, targets)
-
-        target_policies = []
-        for i, next_move in enumerate(targets['next_move']):
-            target_policy = torch.zeros(73, 8, 8)
-            target_policy[int(next_move[2]), int(next_move[0]), int(next_move[1])] = 1
-            target_policies.append(target_policy)
-        target_policies = torch.stack(target_policies)
-
-        target_policies_labels = torch.argmax(torch.flatten(target_policies, start_dim=1, end_dim=-1), dim=1)
-        pred_policies = torch.flatten(pred_policies, start_dim=1, end_dim=-1)
-
-        policy_loss = loss_fn_policy(pred_policies, target_policies_labels.to(device))
-        value_loss = loss_fn_value(pred_values, torch.argmax(targets['result'], dim=1))
-        total_loss = 0.99 * policy_loss + 0.01 * value_loss
-
-        writer.add_scalar('loss/train', total_loss, current_step)
-        writer.add_scalar('lr', optimizer.defaults['lr'], current_step)
-
-        optimizer.zero_grad()
-        total_loss.backward()
-        optimizer.step()
-        scheduler.step(total_loss)  # must call this after the optimizer step
-
-        if current_step % 5000 == 0:
-            torch.save({'model': model.state_dict(),
-                        'optimizer': optimizer.state_dict(),
-                        "global_step": current_step},
-                       str(current_step) + "_steps.tar")
-
-        if current_step == num_steps:
-            break
 
 
 def python_chess_ex():
