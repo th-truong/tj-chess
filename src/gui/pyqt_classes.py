57a152230dcc7b8003a48a23be459ac1ef783dcc
Josh Heinrichs <joshiheinrichs@gmail.com>
2020-11-14 20:58:26 -0600

    Split viewer and player tabs into subclasses

--- src/gui/pyqt_classes.py
+++ src/gui/pyqt_classes.py
@@ -33,213 +33,212 @@ class chessTabs(QWidget):
     def __init__(self, parent, lichess_db, engine):
         super(QWidget, self).__init__(parent)
 
-        self.lichess_db = lichess_db
+        self.tabs = QTabWidget()
+        viewer_tab = ViewerTab(self, lichess_db)
+        player_tab = PlayerTab(self, engine)
+
+        self.tabs.addTab(viewer_tab, "Viewer")
+        self.tabs.addTab(player_tab, "Player")
+
+        layout = QGridLayout(self)
+        layout.addWidget(self.tabs)
+        self.setLayout(layout)
+
+    def wheelEvent(self, event: QWheelEvent):
+        tab = self.tabs.currentWidget()
+        if event.angleDelta().y() > 0:
+            tab.fwd_btn_click()
+        elif event.angleDelta().y() < 0:
+            tab.back_btn_click()
+
+
+class PlayerTab(QWidget):
+    def __init__(self, parent, engine):
+        super(QWidget, self).__init__(parent)
+
         self.engine = engine
 
-        self.layout = QGridLayout(self)
+        layout = QGridLayout(self)
 
-        self.tabs = QTabWidget()
-        self.viewer_tab = QWidget()
-        self.player_tab = QWidget()
+        # SVG display for board
+        self.svg_widget = QSvgWidget(parent=self)
+        self.svg_widget.setGeometry(10, 10, 1000, 1000)
+        layout.addWidget(self.svg_widget, 1, 0, 8, 8)
+        self.board = chess.Board()
+        self.paint_board()
+        # keeps track of current move number in game
+        self.move_counter = 0
 
-        self.tabs.addTab(self.viewer_tab, "Viewer")
-        self.tabs.addTab(self.player_tab, "Player")
+        # textbox for entering move
+        self.move_input = QLineEdit(self)
+        layout.addWidget(self.move_input, 0, 0, 1, 7)
+        self.move_input.setText("Enter move here.")
+        self.move_input.returnPressed.connect(self.move_push)
 
-        self._initialize_viewer_tab()
-        self._initialize_player_tab()
+        # step back move button
+        self.back_btn = QPushButton("<")
+        self.back_btn.clicked.connect(self.back_btn_click)
+        layout.addWidget(self.back_btn, 9, 2)
 
-        # Add tabs to widget
-        self.layout.addWidget(self.tabs)
-        self.setLayout(self.layout)
+        # step forward (get computer move and play it)
+        self.fwd_btn = QPushButton(">")
+        self.fwd_btn.clicked.connect(self.fwd_btn_click)
+        layout.addWidget(self.fwd_btn, 9, 5)
 
-    def viewer_open_file_btn_click(self):
+        # push move button
+        self.push_move_btn = QPushButton("")
+        self.push_move_btn.clicked.connect(self.move_push)
+        layout.addWidget(self.push_move_btn, 0, 7, 1, 1)
+
+        # textbox displaying current game moves
+        self.current_game_moves_txt = QPlainTextEdit(self)
+        layout.addWidget(self.current_game_moves_txt, 0, 8, 10, 7)
+
+        self.setLayout(layout)
+
+    def paint_board(self):
+        self.board_SVG = chess.svg.board(self.board).encode('UTF-8')
+        self.svg_widget.load(self.board_SVG)
+
+    def move_push(self):
+        try:
+            self.board.push_uci(self.move_input.text())
+            self.move_counter += 1
+
+            self.paint_board()
+            self.move_input.setText("")
+        except ValueError:
+            self.current_game_moves_txt.clear()
+            self.current_game_moves_txt.insertPlainText("Invalid move.")
+
+    def back_btn_click(self):
+        if self.move_counter > 0:
+            # undo last move
+            self.board.pop()
+            # diplay result
+            self.paint_board()
+            # reduce move counter
+            self.move_counter -= 1
+
+    def fwd_btn_click(self):
+        result = self.engine.play(self.board, chess.engine.Limit(time=0.1))
+        if result.move is not None:
+            self.board.push(result.move)
+            self.paint_board()
+            self.move_counter += 1
+
+
+class ViewerTab(QWidget):
+    def __init__(self, parent, lichess_db):
+        super(QWidget, self).__init__(parent)
+        layout = QGridLayout(self)
+
+        self.lichess_db = lichess_db
+
+        self.setLayout(layout)
+        self.current_game = None
+
+        # textbox for locating .pgn file to open
+        self.pgn_file_txt = QLineEdit(parent)
+        self.pgn_file_txt.setText("Enter .pgn path here.")
+        layout.addWidget(self.pgn_file_txt, 0, 0, 1, 7)
+
+        # load file button
+        self.open_file_btn = QPushButton("")
+        self.open_file_btn.clicked.connect(self.open_file_btn_click)
+        layout.addWidget(self.open_file_btn, 0, 7, 1, 1)
+
+        # step back move button
+        self.back_btn = QPushButton("<")
+        self.back_btn.clicked.connect(self.back_btn_click)
+        layout.addWidget(self.back_btn, 9, 2)
+
+        # step Forward move button
+        self.fwd_btn = QPushButton(">")
+        self.fwd_btn.clicked.connect(self.fwd_btn_click)
+        layout.addWidget(self.fwd_btn, 9, 5)
+
+        # SVG display for board
+        self.svg_widget = QSvgWidget(parent=parent)
+        self.svg_widget.setGeometry(10, 10, 1000, 1000)
+        layout.addWidget(self.svg_widget, 1, 0, 8, 8)
+        self.board = chess.Board()
+        self.paint_board()
+
+        # listbox for games in current pgn
+        self.games_list = QListWidget()
+        self.games_list.clicked.connect(self.games_list_click)
+        layout.addWidget(self.games_list, 0, 8, 4, 7)
+
+        # textbox displaying current game moves
+        self.current_game_moves_txt = QPlainTextEdit(parent)
+        layout.addWidget(self.current_game_moves_txt, 4, 8, 6, 7)
+
+    def games_list_click(self, qmodelindex):
+        # set game as current selected game from list and display it
+        self.current_game = self.games[self.games_list.currentRow()]
+        self.current_game_moves_txt.clear()
+        self.current_game_moves_txt.insertPlainText(str(self.current_game))
+
+        # load in mainline moves
+        self.mainline_moves = [move for move in self.current_game.mainline_moves()]
+        # used to track which move the display is currently at
+        self.move_counter = 0
+
+        # set board display to current game data
+        self.board = self.current_game.board()
+        self.paint_board()
+
+    def open_file_btn_click(self):
         # get file path
         fname = QFileDialog.getOpenFileName(self, 'Open file', str(self.lichess_db), "PGN files (*.pgn)")
         fname = Path(str(fname[0]))
-        self.viewer_pgn_file_txt.setText(str(fname))
+        self.pgn_file_txt.setText(str(fname))
 
-        if Path(self.viewer_pgn_file_txt.text()).exists() and Path(self.viewer_pgn_file_txt.text()).suffix == '.pgn':
+        if Path(self.pgn_file_txt.text()).exists() and Path(self.pgn_file_txt.text()).suffix == '.pgn':
             # open game file
-            pgn_file = open(Path(self.viewer_pgn_file_txt.text()))
-            self.viewer_games = []
+            pgn_file = open(Path(self.pgn_file_txt.text()))
+            self.games = []
             game = chess.pgn.read_game(pgn_file)
             while game is not None:
-                self.viewer_games.append(game)
+                self.games.append(game)
                 game = chess.pgn.read_game(pgn_file)
 
-            # populate list with viewer_games
-            self.viewer_games_list.clear()
-            game_openers = [i.headers['Opening'] for i in self.viewer_games]
+            # populate list with games
+            self.games_list.clear()
+            game_openers = [i.headers['Opening'] for i in self.games]
 
             list_count = 0
             for opener in game_openers:
-                self.viewer_games_list.insertItem(list_count, opener)
+                self.games_list.insertItem(list_count, opener)
                 list_count += 1
 
-    def viewer_games_list_click(self, qmodelindex):
-        # set game as current selected game from list and display it
-        self.viewer_current_game = self.viewer_games[self.viewer_games_list.currentRow()]
-        self.viewer_current_game_moves_txt.clear()
-        self.viewer_current_game_moves_txt.insertPlainText(str(self.viewer_current_game))
-
-        # load in mainline moves
-        self.viewer_mainline_moves = [move for move in self.viewer_current_game.mainline_moves()]
-        # used to track which move the display is currently at
-        self.viewer_move_counter = 0
-
-        # set board display to current game data
-        self.viewer_board = self.viewer_current_game.board()
-        self.viewer_paint_board()
-
-    def viewer_back_btn_click(self):
-        if self.viewer_current_game is None:
-            self.viewer_current_game_moves_txt.clear()
-            self.viewer_current_game_moves_txt.insertPlainText("Need to load a game! Select a .pgn file and then select a game from the list above.")
+    def back_btn_click(self):
+        if self.current_game is None:
+            self.current_game_moves_txt.clear()
+            self.current_game_moves_txt.insertPlainText("Need to load a game! Select a .pgn file and then select a game from the list above.")
         else:
-            if self.viewer_move_counter > 0:
+            if self.move_counter > 0:
                 # undo last move
-                self.viewer_board.pop()
+                self.board.pop()
                 # diplay result
-                self.viewer_paint_board()
+                self.paint_board()
 
-                self.viewer_move_counter -= 1
+                self.move_counter -= 1
 
-    def viewer_fwd_btn_click(self):
-        if self.viewer_current_game is None:
-            self.viewer_current_game_moves_txt.clear()
-            self.viewer_current_game_moves_txt.insertPlainText("Need to load a game! Select a .pgn file and then select a game from the list above.")
+    def fwd_btn_click(self):
+        if self.current_game is None:
+            self.current_game_moves_txt.clear()
+            self.current_game_moves_txt.insertPlainText("Need to load a game! Select a .pgn file and then select a game from the list above.")
         else:
-            if self.viewer_move_counter < len(self.viewer_mainline_moves):
+            if self.move_counter < len(self.mainline_moves):
                 # push the next move
-                self.viewer_board.push(self.viewer_mainline_moves[self.viewer_move_counter])
+                self.board.push(self.mainline_moves[self.move_counter])
                 # display result
-                self.viewer_paint_board()
+                self.paint_board()
 
-                self.viewer_move_counter += 1
+                self.move_counter += 1
 
-    def viewer_paint_board(self):
-        self.viewer_board_SVG = chess.svg.board(self.viewer_board).encode('UTF-8')
-        self.viewer_SVG_widget.load(self.viewer_board_SVG)
-
-    def _initialize_viewer_tab(self):
-        self.viewer_tab.layout = QGridLayout(self.viewer_tab)
-
-        self.viewer_tab.setLayout(self.viewer_tab.layout)
-        self.viewer_current_game = None
-
-        # textbox for locating .pgn file to open
-        self.viewer_pgn_file_txt = QLineEdit(self)
-        self.viewer_pgn_file_txt.setText("Enter .pgn path here.")
-        self.viewer_tab.layout.addWidget(self.viewer_pgn_file_txt, 0, 0, 1, 7)
-
-        # load file button
-        self.viewer_open_file_btn = QPushButton("")
-        self.viewer_open_file_btn.clicked.connect(self.viewer_open_file_btn_click)
-        self.viewer_tab.layout.addWidget(self.viewer_open_file_btn, 0, 7, 1, 1)
-
-        # step back move button
-        self.viewer_back_btn = QPushButton("<")
-        self.viewer_back_btn.clicked.connect(self.viewer_back_btn_click)
-        self.viewer_tab.layout.addWidget(self.viewer_back_btn, 9, 2)
-
-        # step Forward move button
-        self.viewer_fwd_btn = QPushButton(">")
-        self.viewer_fwd_btn.clicked.connect(self.viewer_fwd_btn_click)
-        self.viewer_tab.layout.addWidget(self.viewer_fwd_btn, 9, 5)
-
-        # SVG display for board
-        self.viewer_SVG_widget = QSvgWidget(parent=self)
-        self.viewer_SVG_widget.setGeometry(10, 10, 1000, 1000)
-        self.viewer_tab.layout.addWidget(self.viewer_SVG_widget, 1, 0, 8, 8)
-        self.viewer_board = chess.Board()
-        self.viewer_paint_board()
-
-        # listbox for viewer_games in current pgn
-        self.viewer_games_list = QListWidget()
-        self.viewer_games_list.clicked.connect(self.viewer_games_list_click)
-        self.viewer_tab.layout.addWidget(self.viewer_games_list, 0, 8, 4, 7)
-
-        # textbox displaying current game moves
-        self.viewer_current_game_moves_txt = QPlainTextEdit(self)
-        self.viewer_tab.layout.addWidget(self.viewer_current_game_moves_txt, 4, 8, 6, 7)
-
-    def player_paint_board(self):
-        self.player_board_SVG = chess.svg.board(self.player_board).encode('UTF-8')
-        self.player_SVG_widget.load(self.player_board_SVG)
-
-    def player_move_push(self):
-        try:
-            self.player_board.push_uci(self.player_move_input.text())
-            self.player_move_counter += 1
-
-            self.player_paint_board()
-            self.player_move_input.setText("")
-        except ValueError:
-            self.player_current_game_moves_txt.clear()
-            self.player_current_game_moves_txt.insertPlainText("Invalid move.")
-
-    def player_back_btn_click(self):
-        if self.player_move_counter > 0:
-            # undo last move
-            self.player_board.pop()
-            # diplay result
-            self.player_paint_board()
-            # reduce move counter
-            self.player_move_counter -= 1
-
-    def player_fwd_btn_click(self):
-        result = self.engine.play(self.player_board, chess.engine.Limit(time=0.1))
-        if result.move is not None:
-            self.player_board.push(result.move)
-            self.player_paint_board()
-            self.player_move_counter += 1
-
-    def wheelEvent(self, event: QWheelEvent):
-        if self.tabs.currentIndex() == 0:  # viewer window
-            if event.angleDelta().y() > 0:
-                self.viewer_fwd_btn_click()
-            elif event.angleDelta().y() < 0:
-                self.viewer_back_btn_click()
-        elif self.tabs.currentIndex() == 1:  # palyer window
-            if event.angleDelta().y() > 0:
-                self.player_fwd_btn_click()
-            elif event.angleDelta().y() < 0:
-                self.player_back_btn_click()
-
-    def _initialize_player_tab(self):
-        self.player_tab.layout = QGridLayout(self.player_tab)
-        self.player_tab.setLayout(self.player_tab.layout)
-
-        # SVG display for board
-        self.player_SVG_widget = QSvgWidget(parent=self)
-        self.player_SVG_widget.setGeometry(10, 10, 1000, 1000)
-        self.player_tab.layout.addWidget(self.player_SVG_widget, 1, 0, 8, 8)
-        self.player_board = chess.Board()
-        self.player_paint_board()
-        # keeps track of current move number in game
-        self.player_move_counter = 0
-
-        # textbox for entering move
-        self.player_move_input = QLineEdit(self)
-        self.player_tab.layout.addWidget(self.player_move_input, 0, 0, 1, 7)
-        self.player_move_input.setText("Enter move here.")
-        self.player_move_input.returnPressed.connect(self.player_move_push)
-
-        # step back move button
-        self.player_back_btn = QPushButton("<")
-        self.player_back_btn.clicked.connect(self.player_back_btn_click)
-        self.player_tab.layout.addWidget(self.player_back_btn, 9, 2)
-
-        # step forward (get computer move and play it)
-        self.player_fwd_btn = QPushButton(">")
-        self.player_fwd_btn.clicked.connect(self.player_fwd_btn_click)
-        self.player_tab.layout.addWidget(self.player_fwd_btn, 9, 5)
-
-        # push move button
-        self.player_push_move_btn = QPushButton("")
-        self.player_push_move_btn.clicked.connect(self.player_move_push)
-        self.player_tab.layout.addWidget(self.player_push_move_btn, 0, 7, 1, 1)
-
-        # textbox displaying current game moves
-        self.player_current_game_moves_txt = QPlainTextEdit(self)
-        self.player_tab.layout.addWidget(self.player_current_game_moves_txt, 0, 8, 10, 7)
+    def paint_board(self):
+        self.board_SVG = chess.svg.board(self.board).encode('UTF-8')
+        self.svg_widget.load(self.board_SVG)
