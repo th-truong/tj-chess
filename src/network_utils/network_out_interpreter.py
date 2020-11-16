cc1e1f9617e25b4c4b8462c51526ec62ba6aafcb
Josh Heinrichs <joshiheinrichs@gmail.com>
2020-11-15 20:32:30 -0600

    Hook interpreter test into pytest

--- src/network_utils/network_out_interpreter.py
+++ src/network_utils/network_out_interpreter.py
@@ -154,24 +154,3 @@ class NetInterpreter():
         all_moves = queen_moves + knight_moves + pawn_underpromos
 
         return all_moves
-
-
-def test_interpreter(interpreter):
-    interpreter.set_colour_to_play("white")
-    print("Testing White, errors:")
-    for i in range(0, 8):
-        for j in range(0, 8):
-            for k in range(0, 73):
-                move = interpreter.interpret_net_move(i, j, k)
-                if move != "INVALID":
-                    if (i, j, k) != interpreter.interpret_UCI_move(move):
-                        print(i, j, k)
-    interpreter.set_colour_to_play("black")
-    print("Testing Black, errors:")
-    for i in range(0, 8):
-        for j in range(0, 8):
-            for k in range(0, 73):
-                move = interpreter.interpret_net_move(i, j, k)
-                if move != "INVALID":
-                    if (i, j, k) != interpreter.interpret_UCI_move(move):
-                        print(i, j, k)
