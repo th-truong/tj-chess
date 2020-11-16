c1b313aa04e21e57e94751538352e66a456d1307
Josh Heinrichs <joshiheinrichs@gmail.com>
2020-11-15 21:02:07 -0600

    Combine black and white loops

--- src/network_utils/test/test_network_out_interpreter.py
+++ src/network_utils/test/test_network_out_interpreter.py
@@ -4,23 +4,13 @@ from network_utils.network_out_interpreter import NetInterpreter
 def test_interpreter_round_trip():
     interpreter = NetInterpreter()
 
-    interpreter.set_colour_to_play("white")
-    print("Testing White, errors:")
-    for i in range(0, 8):
-        for j in range(0, 8):
-            for k in range(0, 73):
-                move = interpreter.interpret_net_move(i, j, k)
-                # TODO: we should explicity test for invalid moves
-                if move == 'INVALID':
-                    continue
-                assert (i, j, k) == interpreter.interpret_UCI_move(move)
-
-    interpreter.set_colour_to_play("black")
-    print("Testing Black, errors:")
-    for i in range(0, 8):
-        for j in range(0, 8):
-            for k in range(0, 73):
-                move = interpreter.interpret_net_move(i, j, k)
-                if move == 'INVALID':
-                    continue
-                assert (i, j, k) == interpreter.interpret_UCI_move(move)
+    for color in ["white", "black"]:
+        interpreter.set_colour_to_play(color)
+        for i in range(0, 8):
+            for j in range(0, 8):
+                for k in range(0, 73):
+                    move = interpreter.interpret_net_move(i, j, k)
+                    # TODO: we should explicity test for invalid moves
+                    if move == 'INVALID':
+                        continue
+                    assert (i, j, k) == interpreter.interpret_UCI_move(move)
